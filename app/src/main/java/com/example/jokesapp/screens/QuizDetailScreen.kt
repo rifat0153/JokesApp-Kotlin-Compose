2d2960f1b86f77ad18798d669826efe97dc9890b
Mahbubur Rahman <rifat0153@gmail.com>
2022-02-13 11:11:04 +0000

    Hilt INIT

---
+++ app/src/main/java/com/example/jokesapp/screens/QuizDetailScreen.kt
@@ -0,0 +1,29 @@
+package com.example.jokesapp.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@Destination
+@Composable
+fun QuizDetailScreen(
+    navigator: DestinationsNavigator,
+    name: String = ""
+) {
+
+    Column() {
+        Text(text = "Quiz Detail Screen $name")
+
+        Button(
+            onClick = {
+                navigator.navigateUp()
+            }
+        ) {
+            Text(text = "Goto Quiz Screen")
+        }
+    }
+
+}
--- app/src/main/java/com/example/jokesapp/screens/QuizScreen.kt
+++ app/src/main/java/com/example/jokesapp/screens/QuizScreen.kt
@@ -1,14 +1,28 @@
 package com.example.jokesapp.screens
 
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import com.example.jokesapp.screens.destinations.QuizDetailScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @Destination
 @Composable
-fun QuizScreen() {
+fun QuizScreen(
+    navigator: DestinationsNavigator
+) {
 
     Text(text = "Quiz Screen")
+
+    Button(
+        onClick = {
+            navigator.navigate(QuizDetailScreenDestination(name = "HAHA"))
+        }
+    ) {
+        Text(text = "Goto Quiz Screen")
+    }
     
 }
+
 
