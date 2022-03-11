791a51621929578e67e1e64ee130d1652441927d
Mahbubur Rahman <rifat0153@gmail.com>
2022-03-11 22:53:33 +0000

    Stacked image tile created

--- app/src/main/java/com/example/jokesapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/jokesapp/presentation/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.jokesapp.presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -12,10 +13,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.jokesapp.R
 import com.example.jokesapp.presentation.common.MyButton
 import com.example.jokesapp.presentation.common.MyOutlinedButton
 import com.example.jokesapp.presentation.destinations.QuizScreenDestination
@@ -37,7 +41,7 @@ fun HomeScreen(
 
         ChipImageTile()
 
-        Text(text = "HomeScreen")
+        Text(text = "HomeScreen", style = TextStyle(color = Color.Black))
 
         MyButton(text = "Play now", onClick = {
             navController.navigate(QuizScreenDestination)
@@ -58,26 +62,56 @@ fun HomeScreen(
 
 @Composable
 fun ChipImageTile(modifier: Modifier = Modifier) {
-    Column(
-        modifier = Modifier
+    Box(
+        Modifier
             .fillMaxWidth()
+            .height(200.dp)
             .padding(kPadding * 2)
-            .clip(shape = RoundedCornerShape(kRadius * 2))
-            .background(Color.Magenta)
     ) {
-        Column(modifier = Modifier.padding(kPadding * 2)) {
-            Icon(
-                imageVector = Icons.Filled.PlayArrow, contentDescription = "icon",
-                modifier = Modifier.size(30.dp)
-            )
-            Text(text = "level 2")
-            Text(
-                text = "Science", style = TextStyle(
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 20.sp
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(kRadius * 2))
+                .background(Color.Magenta)
+                .align(Alignment.BottomCenter)
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .padding(kPadding * 2)
+
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.PlayArrow, contentDescription = "icon",
+                    modifier = Modifier.size(30.dp)
                 )
-            )
+                Text(text = "level 2")
+                Text(
+                    text = "Science", style = TextStyle(
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+                )
+            }
+
         }
+
+
+        Image(
+            painter = painterResource(R.drawable.air_hot_balloon),
+            contentDescription = "Icon Image",
+            modifier = Modifier
+                .height(120.dp)
+                .width(120.dp).padding(end = 16.dp)
+                .align(Alignment.TopEnd)
+        )
     }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+fun JokeImageTilePreview() {
+    ChipImageTile()
 }
